ab41c3b7e317f58dcc8b10fba33d1c9eb7f08158
Theo SAVALE <tsavale@e2r10p20.42.fr>
2019-12-18 11:07:27 +0100

    bolos

---
+++ ft_strdup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsavale <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/22 14:07:57 by tsavale           #+#    #+#             */
+/*   Updated: 2019/10/24 14:26:30 by tsavale          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*cpy;
+	int		i;
+
+	i = 0;
+	if (!(cpy = (char*)malloc(sizeof(char) * ft_strlen(s1) + 1)))
+		return (NULL);
+	while (s1[i])
+	{
+		cpy[i] = s1[i];
+		i++;
+	}
+	cpy[i] = '\0';
+	return (cpy);
+}
